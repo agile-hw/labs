69a5c06ae531fed2612c6fc7838adbaa92c1b366
Scott Beamer <sbeamer@ucsc.edu>
2025-04-04 16:10:30 -0700

    bump scala to 2.13.14 and almond to 0.14.0-RC15

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ python3 -m virtualenv chisel_nb_env
 source chisel_nb_env/bin/activate
 pip3 install jupyterlab
 curl -L -o coursier https://git.io/coursier-cli && chmod +x coursier
-SCALA_VERSION=2.13.10 ALMOND_VERSION=0.13.14
+SCALA_VERSION=2.13.14 ALMOND_VERSION=0.14.0-RC15
 ./coursier bootstrap -r jitpack \
     -i user -I user:sh.almond:scala-kernel-api_$SCALA_VERSION:$ALMOND_VERSION \
     sh.almond:scala-kernel_$SCALA_VERSION:$ALMOND_VERSION \
